9903a165bc3f2324f142f9f0734b972f426e6de5
Gustavo Barros Dutra Santos <gustavo.dutra11@gmail.com>
2024-10-03 14:57:10 -0300

    feat: add routes of http simple request do app.py

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify
 from services.webhook_service import WebhookService
+from services.requests_service import RequestsService
 import os
 
 app = Flask(__name__)
 
 # Inicializa o serviço de webhook
 webhook_service = WebhookService(app)
+
+# Inicializa o serviço de requests
+requests_service = RequestsService()
+
+@app.route('/api/v1/get-suitability/<account_number>', methods=['GET'])
+def get_suitability(account_number):
+    suitability = requests_service.get_suitability(account_number)
+    return jsonify(suitability)
+
+@app.route('/api/v1/get-registration-data/<account_number>', methods=['GET'])
+def get_registration_data(account_number):
+    registration_data = requests_service.get_registration_data(account_number)
+    return jsonify(registration_data)
+
+@app.route('/api/v1/get-account-base', methods=['GET'])
+def get_account_base():
+    account_base = requests_service.get_account_base()
+    return jsonify(account_base)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
