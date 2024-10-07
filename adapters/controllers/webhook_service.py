1e37e0806e01f39f34255cc4038dea8c6ac5bab8
Gustavo Barros Dutra Santos <gustavo.dutra11@gmail.com>
2024-10-07 18:08:10 -0300

    add new webhook endpoints

--- adapters/controllers/webhook_service.py
+++ adapters/controllers/webhook_service.py
@@ -29,27 +29,66 @@ class WebhookService:
         return jsonify({"status": "success", "message": "Event processed", "url": url}), 200
 
     def register_routes(self):
+
+        ## Relatórios Gerenciais
         @self.app.route('/api/v1/rg-tir-mensal-parceiro', methods=['POST'])
-        def webhook_monthly_tir():
+        def monthly_tir():
             return self.log_and_respond("RG - TIR_MENSAL")
 
         @self.app.route('/api/v1/rg-posicoes', methods=['POST'])
-        def webhook_positions():
+        def positions():
             return self.log_and_respond("RG - POSICOES")
 
         @self.app.route('/api/v1/rg-base-btg', methods=['POST'])
-        def webhook_base_btg():
+        def base_btg():
             return self.log_and_respond("RG - BASE BTG")
 
         @self.app.route('/api/v1/rg-nnm-gerencial', methods=['POST'])
-        def webhook_nnm_gerencial():
+        def nnm_gerencial():
             return self.log_and_respond("RG - NNM GERENCIAL")
 
         @self.app.route('/api/v1/rg-fundos', methods=['POST'])
-        def webhook_fundos():
+        def fundos():
             return self.log_and_respond("RG - FUNDOS")
         
-        @self.app.route('/api/v1/webhook-receiver', methods=['POST'])
-        def webhook_receiver():
-            return self.log_and_respond("Generic Webhook")
+        @self.app.route('api/v1/rg-cra-cri', methods=['POST'])
+        def cra_cri():
+            return self.log_and_respond("RG - CRA/CRI")
+        
+        ## Rentabilidade Diária
+        @self.app.route('/api/v1/daily-profit', methods=['POST'])
+        def daily_profit():
+            return self.log_and_respond("Daily Profit")
+        
+        @self.app.route('/api/v1/daily-profit-by-date', methods=['POST'])
+        def daily_profit_by_date():
+            return self.log_and_respond("Daily Profit by Date")
+
+        ## Relatórios de Custódia
+        @self.app.route('/api/v1/custody', methods=['POST'])
+        def custody():
+            return self.log_and_respond("Custody")
+        
+        @self.app.route('/api/v1/custody-by-date', methods=['POST'])
+        def custody_by_date():
+            return self.log_and_respond("Custody by Date")
+        
+        ## Relatório STVM New NET Money
+        @self.app.route('/api/v1/stvm', methods=['POST'])
+        def stvm():
+            return self.log_and_respond("STVM")
+ 
+        ## Comissoes dos ultimos 4 dias
+        @self.app.route('/api/v1/comissions', methods=['POST'])
+        def commissions():
+            return self.log_and_respond("Commissions")
+ 
+        ## Rentabilidade Mensal
+        @self.app.route('/api/v1/monthly-customer-profit', methods=['POST'])
+        def monthly_customer_profit():
+            return self.log_and_respond("Monthly Customer Profit")
+        
+        @self.app.route('/api/v1/monthly-product-profit', methods=['POST'])
+        def monthly_product_profit():
+            return self.log_and_respond("Monthly Product Profit")
 
