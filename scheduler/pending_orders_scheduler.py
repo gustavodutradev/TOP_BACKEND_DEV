7debb4f0fbf2e6a53a84d0cb58c16d5ddc114675
Gustavo Barros Dutra Santos <gustavo.dutra11@gmail.com>
2024-10-15 11:55:48 -0300

    feat: create scheduler file

---
+++ scheduler/pending_orders_scheduler.py
@@ -0,0 +1,30 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
+import pytz
+import requests
+
+
+class PendingOrdersScheduler:
+    def __init__(self, orders_service):
+        self.orders_service = orders_service
+        self.scheduler = BackgroundScheduler(
+            timezone=pytz.timezone("America/Sao_Paulo")
+        )
+        self.configure_jobs()
+
+    def _configure_jobs(self):
+        """Agenda o o fluxo de ordens pendentes"""
+        self.scheduler.add_job(self._send_pendind_orders, "cron", hour=12, minute=0)
+        self.scheduler.add_job(self._send_pendind_orders, "cron", hour=16, minute=0)
+        self.scheduler.start()
+
+    def _send_pending_orders(self):
+        """Chama o serviço para verificar e enviar ordens pendentes"""
+        print(f"Tarefa disparada em: {datetime.now()}")
+        try:
+            requests.post(
+                "https://topbackenddev-production.up.railway.app/api/v1/orders"
+            )
+        except requests.RequestException as e:
+            print(f"Erro ao chamar o controller: {str(e)}")
+            return
