c67503f633e51199d2d87237e601d2a54e9da0bd
Gustavo Barros Dutra Santos <gustavo.dutra11@gmail.com>
2024-10-17 15:32:56 -0300

    feat: implement customer monthly profit, but commented

---
+++ adapters/controllers/monthly_customer_profit_controller.py
@@ -0,0 +1,10 @@
+# from core.services.monthly_customer_profit_service import MonthlyCustomerProfitService
+# from flask import jsonify
+
+# class MonthlyCustomerProfitController:
+#     def __init__(self):
+#         self.monthly_customer_profit_service = MonthlyCustomerProfitService()
+
+#     def get_customer_montlhy_profit(self):
+#         monthly_profit = self.monthly_customer_profit_service.getProfitByPeriod()
+#         return jsonify(monthly_profit)
