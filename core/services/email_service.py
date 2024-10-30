3c21000af953dd88cc5035f142dcfe4df1dc482e
Gustavo Barros Dutra Santos <gustavo.dutra11@gmail.com>
2024-10-30 15:03:27 -0300

    refactor: use compliance email to send emails

--- core/services/email_service.py
+++ core/services/email_service.py
@@ -11,7 +11,7 @@ class EmailService:
         if isinstance(to_emails, str):
             to_emails = [email.strip() for email in to_emails.split(",")]
         email = Mail(
-            from_email="gustavobdsdev@gmail.com",
+            from_email="compliance@topinvgroup.com",
             to_emails=to_emails,
             subject=subject,
             html_content=content if is_html else None,
