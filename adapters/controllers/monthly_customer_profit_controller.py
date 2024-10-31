8a64ae479d8f3de479c16f566a5fcb6ecee818ac
Gustavo Barros Dutra Santos <gustavo.dutra11@gmail.com>
2024-10-31 14:24:44 -0300

    feat: uncomment code and implement improvements for monthly profit webhook

--- adapters/controllers/monthly_customer_profit_controller.py
+++ adapters/controllers/monthly_customer_profit_controller.py
@@ -1,10 +1,82 @@
-# from core.services.monthly_customer_profit_service import MonthlyCustomerProfitService
-# from flask import jsonify
+from flask import jsonify, request
+from core.services.monthly_customer_profit_service import MonthlyCustomerProfitService
+from core.services.token_service import TokenService
+from utils.logging_requests import Logger
+import traceback
 
-# class MonthlyCustomerProfitController:
-#     def __init__(self):
-#         self.monthly_customer_profit_service = MonthlyCustomerProfitService()
 
-#     def get_customer_montlhy_profit(self):
-#         monthly_profit = self.monthly_customer_profit_service.getProfitByPeriod()
-#         return jsonify(monthly_profit)
+class MonthlyCustomerProfitController:
+    def __init__(self, app):
+        self.monthly_customer_profit_service = MonthlyCustomerProfitService()
+        self.token_service = TokenService()
+        self.app = app
+        self.register_routes()
+        self.logger = Logger(app)
+
+    def register_routes(self):
+        @self.app.route("/api/v1/monthly-customer-profit", methods=["POST"])
+        def handler():
+            """Lida com a solicitação inicial e também processa webhooks."""
+            try:
+                if request.is_json:
+                    data = request.get_json(silent=True)
+
+                    if "response" in data:
+                        self.logger.log_and_respond("Webhook recebido.")
+                        return self._process_webhook(data)
+                self.logger.log_and_respond(
+                    "Iniciando requisição de rentabilidade mensal dos clientes."
+                )
+                response = self.monthly_customer_profit_service.getProfitByPeriod()
+
+                if response:
+                    return (
+                        jsonify(
+                            {
+                                "message": "Requisição aceita. Aguardando processamento via webhook."
+                            }
+                        ),
+                        202,
+                    )
+
+                return (
+                    jsonify(
+                        {
+                            "error": "Falha ao iniciar a requisição de rentabilidade mensal dos clientes."
+                        }
+                    ),
+                    500,
+                )
+
+            except Exception as e:
+                self.logger.logger.error(f"Erro na requisição: {str(e)}")
+                self.logger.logger.error(traceback.format_exc())
+                return jsonify({"error": "Internal server error"}), 500
+
+    def _process_webhook(self, data):
+        """Processa o payload enviado pela API via webhook."""
+        try:
+
+            csv_url = data.get("response", {}).get("url")
+            if not csv_url:
+                self.logger.logger.error("URL do CSV não encontrada no payload.")
+                return jsonify({"error": "CSV URL not found."}), 400
+
+            pending_orders = self.orders_service.process_csv_from_url(csv_url)
+            if not pending_orders:
+                self.logger.logger.info("Nenhuma ordem pendente encontrada.")
+                self.orders_service.send_empty_pending_orders_email()
+                return jsonify({"message": "Nenhuma ordem pendente encontrada."}), 204
+
+            self.logger.logger.info(f"Ordens pendentes encontradas: {pending_orders}")
+
+            self.orders_service.send_pending_orders_email(pending_orders)
+
+            return jsonify(pending_orders), 200
+        except Exception as e:
+            self.logger.logger.error(
+                f"Erro ao processar o payload do webhook: {str(e)}"
+            )
+            self.logger.logger.error(traceback.format_exc())
+            return jsonify({"error": "Internal server error"}), 500
+
--- core/services/monthly_customer_profit_service.py
+++ core/services/monthly_customer_profit_service.py
@@ -1,49 +1,51 @@
-# from core.services.config_service import ConfigService
-# import requests
-# from core.services.zip_service import ZipService
+from core.services.config_service import ConfigService
+import requests
+from core.services.zip_service import ZipService
 
 
-# class MonthlyCustomerProfitService:
-#     """Classe para requisitar rentabilidade mensal do cliente."""
+class MonthlyCustomerProfitService:
+    """Classe para requisitar rentabilidade mensal do cliente."""
 
-#     def __init__(self) -> None:
-#         self.config_service = ConfigService()
+    def __init__(self) -> None:
+        self.config_service = ConfigService()
 
-#     def getProfitByPeriod(self):
-#         """Requisita rentabilidade mensal do cliente."""
-#         endpoint = "/api-partner-report-hub/api/v1/report/customer-profitability"
-#         url = f"{self.config_service._base_url}{endpoint}"
-#         period = {"referenceMonth": "x", "referenceYear": "y"}
+    def getProfitByPeriod(self):
+        """Requisita rentabilidade mensal do cliente."""
+        endpoint = "/api-partner-report-hub/api/v1/report/customer-profitability"
+        url = f"{self.config_service._base_url}{endpoint}"
+        # period = {"referenceMonth": "x", "referenceYear": "y"}
 
-#         try:
-#             headers = self.config_service.get_headers()
-#             response = requests.get(url, json=period, headers=headers)
+        try:
+            headers = self.config_service.get_headers()
+            response = requests.get(url, headers=headers)
 
-#             if response.status_code == 202:
-#                 print("Requisição aceita. Aguarde o webhook para processamento.")
-#                 return True
+            if response.status_code == 202:
+                print("Requisição aceita. Aguarde o webhook para processamento.")
+                return True
 
-#             print(f"Erro na requisição: {response.status_code} - {response.text}")
-#             return None
+            print(f"Erro na requisição: {response.status_code} - {response.text}")
+            return None
 
-#         except requests.RequestException as e:
-#             print(f"Erro na requisição: {str(e)}")
-#             return None
+        except requests.RequestException as e:
+            print(f"Erro na requisição: {str(e)}")
+            return None
 
-#     def process_csv_from_url(self, csv_url):
-#         """Realiza o download do CSV zipado e extrai as informações"""
-#         try:
-#             zip_response = requests.get(csv_url)
-#             if zip_response.status_code != 200:
-#                 raise Exception(
-#                     f"Erro ao baixar o arquivo ZIP: {zip_response.status_code}"
-#                 )
+    def process_csv_from_url(self, csv_url):
+        """Realiza o download do CSV zipado e extrai as informações"""
+        try:
+            zip_response = requests.get(csv_url)
+            if zip_response.status_code != 200:
+                raise Exception(
+                    f"Erro ao baixar o arquivo ZIP: {zip_response.status_code}"
+                )
 
-#             zip_service = ZipService()
+            zip_service = ZipService()
 
-#             unziped_file = zip_service.unzip_csv_reader(zip_response)
-#             return unziped_file
+            unziped_file = zip_service.unzip_csv_reader(zip_response)
 
-#         except requests.RequestException as e:
-#             print(f"Erro na requisição: {str(e)}")
-#             return None
+            # for reader in unziped_file ........
+            return unziped_file
+
+        except requests.RequestException as e:
+            print(f"Erro na requisição: {str(e)}")
+            return None
