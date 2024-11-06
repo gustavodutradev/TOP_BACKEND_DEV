906880518e60d8117841da9d2901c40bbe76b040
Gustavo Barros Dutra Santos <gustavo.dutra11@gmail.com>
2024-11-06 12:59:26 -0300

    refactor: change url name

--- adapters/controllers/management_reports/funds_controller.py
+++ adapters/controllers/management_reports/funds_controller.py
@@ -18,7 +18,7 @@ class FundsController:
     def register_routes(self) -> None:
         """Register the API routes for the controller."""
         self.app.add_url_rule(
-            "/api/v1/rg-funds", "rg_funds_handler", self.handler, methods=["POST"]
+            "/api/v1/rg-fundos", "rg_funds_handler", self.handler, methods=["POST"]
         )
 
     def handler(self) -> Tuple[Dict[str, Any], int]:
